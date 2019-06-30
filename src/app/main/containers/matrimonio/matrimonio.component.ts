d623603855bffb9c913f78936fab82538894ad70
Elena Garrone <elena.garrone@landg.com>
2019-06-30 16:50:27 +0100

    first commit -m

---
+++ src/app/main/containers/matrimonio/matrimonio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matrimonio',
+  templateUrl: './matrimonio.component.html',
+  styleUrls: ['./matrimonio.component.scss']
+})
+export class MatrimonioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main.component';
 import { RouterModule } from '@angular/router';
+
+import { NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { MainComponent } from './main.component';
 import { routes } from './main.routes';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MatrimonioComponent } from './containers/matrimonio/matrimonio.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
-const components = [ MainComponent, NavigationComponent ]
+const components = [ MainComponent, NavigationComponent, BannerComponent, MatrimonioComponent, TabsComponent ]
 
 @NgModule({
   declarations: [ ...components ],
   imports: [
     CommonModule,
+    NgbTabsetModule,
     RouterModule.forRoot(routes)
   ]
 })
