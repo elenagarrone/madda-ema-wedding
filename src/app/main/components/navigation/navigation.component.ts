407a3add230ff317b0e427f9bddb6909a5c2d9d8
Elena Garrone <elena.garrone@landg.com>
2019-07-03 00:02:20 +0100

    Add new hoem icon

--- src/app/main/components/navigation/navigation.component.ts
+++ src/app/main/components/navigation/navigation.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class NavigationComponent {
   links = [
     {
+      isHome: true,
       text: 'Matrimonio',
       href: '#matrimonio-section'
     },
