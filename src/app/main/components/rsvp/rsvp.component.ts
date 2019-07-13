4b3a93c24189d66e4def16d718a7cb86a2d5d276
Elena Garrone <elena.garrone@landg.com>
2019-07-13 19:56:51 +0100

    Add form and style

--- src/app/main/components/rsvp/rsvp.component.ts
+++ src/app/main/components/rsvp/rsvp.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormArray } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-rsvp',
   templateUrl: './rsvp.component.html',
-  styleUrls: ['./rsvp.component.scss']
+  styleUrls: ['./rsvp.component.scss'],
 })
 export class RsvpComponent implements OnInit {
-  rsvpForm: FormArray;
+  rsvpForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.rsvpForm = this.formBuilder.array([ this.addPerson() ])
+    this.rsvpForm = this.formBuilder.group({
+      people: this.formBuilder.array([ this.formBuilder.group({ name: '' }) ]),
+    });
+  }
+
+  get people() {
+    return this.rsvpForm.get('people') as FormArray;
+  }
+
+  addPeople() {
+    this.people.push(this.formBuilder.group({ name: '' }));
+  }
+
+  deletePeople(index) {
+    this.people.removeAt(index);
   }
 }
